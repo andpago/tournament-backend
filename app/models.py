dc7d439374588840593d3392cf800f56ea9e0565
andpago <andpago@gmail.com>
2018-05-04 00:15:06 +0300

    tournament model

--- app/models.py
+++ app/models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    is_deleted = models.BooleanField(default=False)
--- round/models.py
+++ round/models.py
@@ -1,7 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
+
+
+class Tournament(models.Model):
+    title = models.TextField(max_length=255)
+    is_deleted = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Round(models.Model):
     title = models.TextField(max_length=255)
     is_deleted = models.BooleanField(default=False)
-
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)

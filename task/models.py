4985815bb783a43d5514c023a78d8188dc8d1c69
andpago <andpago@gmail.com>
2018-05-04 00:01:44 +0300

    solution model

--- task/models.py
+++ task/models.py
@@ -5,5 +5,13 @@ User = get_user_model()
 
 
 class Task(models.Model):
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) # users cannot really be deleted
-    text = models.TextField() # for now this will be just text
+    # users cannot really be deleted, so this setting is irrelevant
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    text = models.TextField()  # for now this will be just text
+
+
+class Solution(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    correct = models.NullBooleanField(null=True)  # null until checked
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
